250dac404eed24b2c5559a0d0a6e25e09b169910
Adrian Nicolau <adrian.nicolau74@gmail.com>
2015-04-25 16:34:16 +0300

    Switch from TUN to TAP.

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
-from pytun import TunTapDevice
+from pytun import TunTapDevice, IFF_TAP
 from binascii import hexlify
 
+import struct
+
+
 if __name__ == '__main__':
-    tun = TunTapDevice(name='ipsec-tun')
-    tun.up()
-    tun.persist(True)
+    tap = TunTapDevice(name='ipsec-tap', flags=IFF_TAP)
+    tap.up()
+    tap.persist(True)
 
     while True:
         try:
-            buf = tun.read(tun.mtu)
-            print hexlify(buf[4:])
-            IPpayload = buf[4:]
+            buf = tap.read(tap.mtu)
+            print 'old', hexlify(buf)
+            # Valeri @ studentpub
+            new_addr = struct.pack('4B', 192, 168, 10, 174)
+            new_buf = buf[:34] + new_addr + buf[38:]
+            print 'new', hexlify(new_buf)
+            print
+            tap.write(new_buf)
             # TODO encrypt buf
             # TODO send to wlan0
             # TODO enable routing
         except KeyboardInterrupt:
-            tun.close()
+            tap.close()
--- set_tap.sh
+++ set_tap.sh
@@ -2,5 +2,6 @@
 
 ip tuntap add mode tap dev ipsec-tap
 ip link set dev ipsec-tap up
-ip addr add 192.168.53.53 dev ipsec-tap
-ip route add 192.168.10.174/32 dev ipsec-tap # Valeri @ studentpub
+ip addr add 192.168.42.42 dev ipsec-tap
+ip route add 42.42.42.42/32 dev ipsec-tap
+# Add arp static entry for 42.42.42.42 with the destination MAC for the IP address in main.py.
