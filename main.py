4f6f9d8e7a99a8e009cec7122d54627ed92c468e
Adrian Nicolau <adrian.nicolau74@gmail.com>
2015-04-25 13:33:11 +0300

    Add TODOs.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
 from pytun import TunTapDevice
+from binascii import hexlify
+
 
 if __name__ == '__main__':
 	tun = TunTapDevice(name='ipsec-tun')
 	tun.up()
+	tun.persist(True)
 
 	while True:
-		buf = tun.read(tun.mtu)
-		print buf
+		try:
+			buf = tun.read(tun.mtu)
+			print hexlify(buf[4:])
+			IPpayload = buf[4:]
+			# TODO encrypt buf
+			# TODO send to wlan0
+			# TODO enable routing			
+		except KeyboardInterrupt:
+			tun.close()
