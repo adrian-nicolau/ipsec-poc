1f6fb7bb0e20cafbe4392bf8a0cafa3e9fa2fbc1
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2015-04-25 13:38:24 +0300

    Change shebang to use python from environment. Fix Indentation.

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from pytun import TunTapDevice
 from binascii import hexlify
 
-
 if __name__ == '__main__':
-	tun = TunTapDevice(name='ipsec-tun')
-	tun.up()
-	tun.persist(True)
+    tun = TunTapDevice(name='ipsec-tun')
+    tun.up()
+    tun.persist(True)
 
-	while True:
-		try:
-			buf = tun.read(tun.mtu)
-			print hexlify(buf[4:])
-			IPpayload = buf[4:]
-			# TODO encrypt buf
-			# TODO send to wlan0
-			# TODO enable routing			
-		except KeyboardInterrupt:
-			tun.close()
+    while True:
+        try:
+            buf = tun.read(tun.mtu)
+            print hexlify(buf[4:])
+            IPpayload = buf[4:]
+            # TODO encrypt buf
+            # TODO send to wlan0
+            # TODO enable routing
+        except KeyboardInterrupt:
+            tun.close()
